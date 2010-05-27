3a39ada0f94347745c8b83fcdb51300e1143256a
Andrey Kutejko <andy128k@gmail.com>
2010-05-28 01:49:03 +0300

    improve building of deb-package.
    added copyright file.

--- make-deb.sh
+++ make-deb.sh
@@ -2,29 +2,29 @@
 
 case `uname -m` in
     x86_64)
-	ARCH=amd64
-	;;
+        ARCH=amd64
+        ;;
     i686)
-	ARCH=i386
-	;;
+        ARCH=i386
+        ;;
     *)
-	echo 'Unknown architecture'
-	exit 1
+        echo 'Unknown architecture'
+        exit 1
 esac
 
 DATE=`date -R`
 VERSION=`date +0.%-y.%-m.%-d`
 
-PREFIX=deb/PassStorage
+PREFIX=deb
 
-rm -rf deb
+rm -rf $PREFIX
 mkdir -p $PREFIX/usr/bin
 cp PassStorage $PREFIX/usr/bin/
 cp -aR share $PREFIX/usr/
 
 mkdir -p $PREFIX/usr/share/applications/
 echo "[Desktop Entry]"                       > $PREFIX/usr/share/applications/PassStorage.desktop
-echo "Version=$VERSION"                     >> $PREFIX/usr/share/applications/PassStorage.desktop
+echo "Version=1.0"                          >> $PREFIX/usr/share/applications/PassStorage.desktop
 echo "Name=PassStorage"                     >> $PREFIX/usr/share/applications/PassStorage.desktop
 echo "GenericName=PassStorage"              >> $PREFIX/usr/share/applications/PassStorage.desktop
 echo "Comment=Secure storage for passwords" >> $PREFIX/usr/share/applications/PassStorage.desktop
@@ -36,26 +36,28 @@ echo "Categories=Utility;"                  >> $PREFIX/usr/share/applications/Pa
 echo "StartupNotify=true"                   >> $PREFIX/usr/share/applications/PassStorage.desktop
 echo "MimeType=application/x-revelation;"   >> $PREFIX/usr/share/applications/PassStorage.desktop
 
-mkdir -p $PREFIX/DEBIAN
+D=DEBIAN
+mkdir -p $PREFIX/$D
 
-echo "Package: PassStorage"                             > $PREFIX/DEBIAN/control
-echo "Version: $VERSION"                               >> $PREFIX/DEBIAN/control
-echo "Maintainer: Andrey Kutejko <andy128k@gmail.com>" >> $PREFIX/DEBIAN/control
-echo "Architecture: $ARCH"                             >> $PREFIX/DEBIAN/control
-echo "Description: Secure storage for passwords"       >> $PREFIX/DEBIAN/control
-echo "Depends: libgtk2.0-0 (>= 2.16)"                  >> $PREFIX/DEBIAN/control
+echo "Package: pass-storage"                                             > $PREFIX/$D/control
+echo "Version: $VERSION"                                                >> $PREFIX/$D/control
+echo "Maintainer: Andrey Kutejko <andy128k@gmail.com>"                  >> $PREFIX/$D/control
+echo "Architecture: $ARCH"                                              >> $PREFIX/$D/control
+echo "Section: misc"                                                    >> $PREFIX/$D/control
+echo "Priority: optional"                                               >> $PREFIX/$D/control
+echo "Description: Password manager"                                    >> $PREFIX/$D/control
+echo " PassStorage manages passwords/secrets and stores them securely." >> $PREFIX/$D/control
+echo "Depends: libc6, libgtk2.0-0 (>= 2.16)"                            >> $PREFIX/$D/control
 
-echo "PassStorage ($VERSION) stable; urgency=medium" > $PREFIX/DEBIAN/changelog
-echo ""                                             >> $PREFIX/DEBIAN/changelog
-echo "* dummy."                                     >> $PREFIX/DEBIAN/changelog
-echo ""                                             >> $PREFIX/DEBIAN/changelog
-echo "-- Andrey Kutejko <andy128k@gmail.com> $DATE" >> $PREFIX/DEBIAN/changelog
+echo "pass-storage ($VERSION) stable; urgency=medium"  > $PREFIX/usr/share/doc/pass-storage/changelog
+echo ""                                               >> $PREFIX/usr/share/doc/pass-storage/changelog
+echo "  * dummy."                                     >> $PREFIX/usr/share/doc/pass-storage/changelog
+echo ""                                               >> $PREFIX/usr/share/doc/pass-storage/changelog
+echo " -- Andrey Kutejko <andy128k@gmail.com>  $DATE" >> $PREFIX/usr/share/doc/pass-storage/changelog
+echo ""                                               >> $PREFIX/usr/share/doc/pass-storage/changelog
+gzip -9 $PREFIX/usr/share/doc/pass-storage/changelog
 
-chown -R root:root $PREFIX/
-
-cd deb
-dpkg-deb -z9 -Zlzma --build PassStorage
-cd ..
-cp deb/PassStorage.deb PassStorage_${VERSION}_${ARCH}.deb
-rm -rf deb
+fakeroot dpkg-deb -Zbzip2 -z9 --build $PREFIX .
+#  -z9 -Zxz
+rm -rf $PREFIX
 
