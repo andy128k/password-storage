fe342dd4674581dd2008abc0c3be1c6848cb53e4
Andrey Kutejko <andy128k@gmail.com>
2011-11-22 01:37:09 +0200

    compress sbcl cores

--- compile.sh
+++ compile.sh
@@ -22,18 +22,18 @@ usage () {
 while getopts "hl:t:" opt
 do
     case $opt in
-	l)
-	    LISP="$OPTARG"
-	    ;;
-	t)
-	    TARGET="$OPTARG"
-	    ;;
-	h)
-	    usage 0
-	    ;;
-	\?)
-	    usage 1
-	    ;;
+        l)
+            LISP="$OPTARG"
+            ;;
+        t)
+            TARGET="$OPTARG"
+            ;;
+        h)
+            usage 0
+            ;;
+        \?)
+            usage 1
+            ;;
     esac
 done
 shift $(($OPTIND - 1))
@@ -45,12 +45,12 @@ fi
 # validate lisp
 case "$LISP" in
     sbcl|ccl)
-	EVAL_KEY=--eval
-	;;
+        EVAL_KEY=--eval
+        ;;
     *)
-	echo "Unknown Common Lisp implementation." >&2
-	exit 1
-	;;
+        echo "Unknown Common Lisp implementation." >&2
+        exit 1
+        ;;
 esac
 
 # detect platform
@@ -59,26 +59,26 @@ case `uname -s` in
         PLATFORM=linux
         EXECUTABLE_SUFFIX=
         case $LISP in
-	    sbcl)
-		LISP_EXECUTABLE=sbcl
-		;;
-	    ccl)
-		LISP_EXECUTABLE=ccl
-		;;
-	esac
-	;;
+            sbcl)
+                LISP_EXECUTABLE=sbcl
+                ;;
+            ccl)
+                LISP_EXECUTABLE=ccl
+                ;;
+        esac
+        ;;
     MINGW32*)
         PLATFORM=win32
-	EXECUTABLE_SUFFIX=.exe
-	case $LISP in
-	    sbcl)
-		LISP_EXECUTABLE=sbcl
-		;;
-	    ccl)
-		LISP_EXECUTABLE=wx86cl
-		;;
-	esac
-	;;
+        EXECUTABLE_SUFFIX=.exe
+        case $LISP in
+            sbcl)
+                LISP_EXECUTABLE=sbcl
+                ;;
+            ccl)
+                LISP_EXECUTABLE=wx86cl
+                ;;
+        esac
+        ;;
     Darwin)
         PLATFORM=darwin
         EXECUTABLE_SUFFIX=
@@ -91,7 +91,7 @@ case `uname -s` in
     *)
         echo 'Unsupported platform' >&2
         exit 1
-	;;
+        ;;
 esac
 
 make_target () {
@@ -103,43 +103,42 @@ make_target () {
     EV="(progn "
     for path in $PATHS
     do
-	EV="$EV (push #p\"$path\" asdf:*central-registry*)"
+        EV="$EV (push #p\"$path\" asdf:*central-registry*)"
     done
 
     for package in $PACKAGES
     do
-    	EV="$EV (asdf:load-system :$package)"
+        EV="$EV (asdf:load-system :$package)"
     done
     EV="$EV)"
 
     case $LISP in
-	sbcl)
-	    if [ -z $ENTRY_POINT ]
-	    then
-    		EV2="(sb-ext:save-lisp-and-die \"$TARGET_NAME$EXECUTABLE_SUFFIX\" :executable t)"
-	    else
-		EV2="(sb-ext:save-lisp-and-die \"$TARGET_NAME$EXECUTABLE_SUFFIX\" :executable t :toplevel '$ENTRY_POINT)"
-	    fi
-	    ;;
-	ccl)
-	    if [ -z $ENTRY_POINT ]
-	    then
-    		EV2="(ccl:save-application \"$TARGET_NAME$EXECUTABLE_SUFFIX\" :prepend-kernel t)"
-	    else
-		EV2="(ccl:save-application \"$TARGET_NAME$EXECUTABLE_SUFFIX\" :prepend-kernel t :toplevel-function '$ENTRY_POINT)"
-	    fi
-	    ;;
+        sbcl)
+            if [ -z $ENTRY_POINT ]
+            then
+                EV2="(sb-ext:save-lisp-and-die \"$TARGET_NAME$EXECUTABLE_SUFFIX\" :compression t :executable t)"
+            else
+                EV2="(sb-ext:save-lisp-and-die \"$TARGET_NAME$EXECUTABLE_SUFFIX\" :compression t :executable t :toplevel '$ENTRY_POINT)"
+            fi
+            ;;
+        ccl)
+            if [ -z $ENTRY_POINT ]
+            then
+                EV2="(ccl:save-application \"$TARGET_NAME$EXECUTABLE_SUFFIX\" :prepend-kernel t)"
+            else
+                EV2="(ccl:save-application \"$TARGET_NAME$EXECUTABLE_SUFFIX\" :prepend-kernel t :toplevel-function '$ENTRY_POINT)"
+            fi
+            ;;
     esac
     $LISP_EXECUTABLE $EVAL_KEY "$EV" $EVAL_KEY "$EV2"
 }
 
 case $TARGET in
     core)
-#	make_target "" "cl-z cl-fad ironclad cl-gtk2-gdk sb-queue cl-gtk2-gtk" lisp-cl-gtk2
-	make_target "" "cl-z cl-fad ironclad cl-gtk2-gdk cl-gtk2-gtk" lisp-cl-gtk2
-	;;
+        make_target "" "cl-z cl-fad ironclad cl-gtk2-gdk cl-gtk2-gtk" lisp-cl-gtk2
+        ;;
     ps)
-	make_target /home/andy/projects/PassStorage/ pass-storage PassStorage pass-storage::main-and-quit
-	;;
+        make_target /home/andy/projects/PassStorage/ pass-storage PassStorage pass-storage::main-and-quit
+        ;;
 esac
 
