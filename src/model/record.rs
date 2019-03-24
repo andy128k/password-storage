9c23d7f85c5544ed51fdce3d175ea99c398181b3
Andrey Kutejko <andy128k@gmail.com>
2019-03-24 22:18:08 +0100

    make search case insensitive

--- src/model/record.rs
+++ src/model/record.rs
@@ -320,9 +320,10 @@ impl Record {
     }
 
     pub fn has_text(&self, text: &str, look_at_secrets: bool) -> bool {
+        let needle = text.to_lowercase();
         for field in &self.record_type.fields {
             if look_at_secrets || !field.field_type.is_secret() {
-                if self.get_field(field).contains(text) {
+                if self.get_field(field).to_lowercase().contains(&needle) {
                     return true;
                 }
             }
