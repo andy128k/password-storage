5e880930f41574f1c4eee4b6c8ff5fea152016dd
Andrey Kutejko <andy128k@gmail.com>
2019-04-17 00:34:15 +0200

    Add AppVeyor build for Windows

--- build.rs
+++ build.rs
@@ -1,12 +1,16 @@
 use std::error::Error;
+use std::path::Path;
 use std::process::Command;
 use std::env;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let out_dir = env::var("OUT_DIR")?;
 
+    let out_path = Path::new(&out_dir).join("icons.gresource").to_path_buf();
+    let out_path = out_path.to_str().ok_or("Cannot build out_path")?;
+
     Command::new("glib-compile-resources")
-        .arg(&format!("--target={}/icons.gresource", out_dir))
+        .arg(&format!("--target={}", out_path))
         .arg("icons.gresource.xml")
         .status()?;
 
