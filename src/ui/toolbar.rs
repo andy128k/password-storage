f70acc35b0a64705f87ca73932b4b6ba6f5deba5
Andrey Kutejko <andy128k@gmail.com>
2021-08-01 16:48:16 +0200

    Preparations to GTK4: Get rid of GtkToolbar

--- src/ui/toolbar.rs
+++ src/ui/toolbar.rs
@@ -1,71 +1,30 @@
-use gtk::prelude::*;
-
 use super::menu::create_add_entity_menu;
 use crate::actions::*;
-use crate::model::record::RECORD_TYPE_GENERIC;
+use crate::utils::toolbar_builder::*;
 
-fn button(label: &str, icon: &str, action: &str) -> gtk::ToolButton {
-    gtk::ToolButton::builder()
-        .label(label)
-        .icon_name(icon)
-        .action_name(action)
-        .has_tooltip(true)
-        .tooltip_text(label)
-        .build()
-}
-
-pub fn create_tool_bar(search_entry: &gtk::Widget) -> gtk::Toolbar {
-    let toolbar = gtk::Toolbar::new();
-
-    toolbar.add(&button("New file", "document-new", "app.new"));
-    toolbar.add(&button("Open file", "document-open", "app.open"));
-    toolbar.add(&button(
-        "Save file",
-        "document-save",
-        &PSAction::ViewMode(ViewModeAction::Save).full_name(),
-    ));
-    toolbar.add(&gtk::SeparatorToolItem::new());
-    toolbar.add(&{
-        let image = gtk::Image::from_icon_name(Some("list-add"), gtk::IconSize::SmallToolbar);
-        let item = gtk::MenuToolButton::new(Some(&image), Some("Add entry"));
-        item.set_menu(&gtk::Menu::from_model(&create_add_entity_menu()));
-        let default_action =
-            PSAction::ViewMode(ViewModeAction::Add(RECORD_TYPE_GENERIC.name.to_string()));
-        item.set_action_name(Some(default_action.full_name().as_ref()));
-        item
-    });
-
-    toolbar.add(&gtk::SeparatorToolItem::new());
-
-    toolbar.add(&{
-        let item = gtk::ToggleToolButton::new();
-        item.set_label(Some("Toggle merge mode"));
-        item.set_icon_name(Some("merge-mode"));
-        item.set_action_name(Some(
+pub fn create_tool_bar(search_entry: &gtk::Widget) -> gtk::Widget {
+    ToolbarBuilder::new()
+        .button("New file", "document-new", "app.new")
+        .button("Open file", "document-open", "app.open")
+        .button(
+            "Save file",
+            "document-save",
+            &PSAction::ViewMode(ViewModeAction::Save).full_name(),
+        )
+        .separator()
+        .menu_button("Add entry", "list-add", &create_add_entity_menu())
+        .separator()
+        .toggle_button(
+            "Toggle merge mode",
+            "merge-mode",
             PSAction::Doc(DocAction::MergeMode).full_name().as_ref(),
-        ));
-        item
-    });
-
-    toolbar.add(&button(
-        "Merge entries",
-        "merge",
-        &PSAction::MergeMode(MergeModeAction::Merge).full_name(),
-    ));
-
-    toolbar.add(&{
-        // expander
-        let item = gtk::ToolItem::new();
-        ToolItemExt::set_expand(&item, true);
-        item
-    });
-
-    toolbar.add(&{
-        // search entry
-        let item = gtk::ToolItem::new();
-        item.add(search_entry);
-        item
-    });
-
-    toolbar
+        )
+        .button(
+            "Merge entries",
+            "merge",
+            &PSAction::MergeMode(MergeModeAction::Merge).full_name(),
+        )
+        .expander()
+        .add(search_entry)
+        .build()
 }
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -2,5 +2,6 @@ pub mod clipboard;
 pub mod menu_builder;
 pub mod promise;
 pub mod string;
+pub mod toolbar_builder;
 pub mod tree;
 pub mod ui;
