5c98783faf4a8b493082e568ab646c5aafce0431
Andrey Kutejko <andy128k@gmail.com>
2021-08-08 22:52:04 +0200

    switch to native open/save dialogs

--- src/ui/dialogs/open_file.rs
+++ src/ui/dialogs/open_file.rs
@@ -3,28 +3,23 @@ use crate::utils::promise::Promise;
 use std::path::PathBuf;
 
 pub async fn open_file(parent_window: &gtk::Window) -> Option<PathBuf> {
-    let dlg = gtk::FileChooserDialog::builder()
+    let dlg = gtk::FileChooserNative::builder()
         .modal(true)
         .title("Open file")
         .transient_for(parent_window)
         .action(gtk::FileChooserAction::Open)
-        .icon_name("password-storage")
-        .window_position(gtk::WindowPosition::CenterOnParent)
         .build();
-    dlg.add_button("_Cancel", gtk::ResponseType::Cancel);
-    dlg.add_button("_Open", gtk::ResponseType::Ok);
-    dlg.set_default_response(gtk::ResponseType::Ok);
 
     let (promise, future) = Promise::<Option<PathBuf>>::new();
     dlg.connect_response(move |dlg, answer| {
         dlg.hide();
-        let result = if answer == gtk::ResponseType::Ok {
+        let result = if answer == gtk::ResponseType::Accept {
             dlg.filename()
         } else {
             None
         };
         promise.fulfill(result);
     });
-    dlg.show_all();
+    dlg.show();
     future.await.unwrap_or(None)
 }
--- src/ui/dialogs/save_file.rs
+++ src/ui/dialogs/save_file.rs
@@ -3,28 +3,23 @@ use crate::utils::promise::Promise;
 use std::path::PathBuf;
 
 pub async fn save_file(parent_window: &gtk::Window) -> Option<PathBuf> {
-    let dlg = gtk::FileChooserDialog::builder()
+    let dlg = gtk::FileChooserNative::builder()
         .title("Save file")
         .modal(true)
         .transient_for(parent_window)
         .action(gtk::FileChooserAction::Save)
-        .icon_name("password-storage")
-        .window_position(gtk::WindowPosition::CenterOnParent)
         .build();
-    dlg.add_button("_Cancel", gtk::ResponseType::Cancel);
-    dlg.add_button("_Save", gtk::ResponseType::Ok);
-    dlg.set_default_response(gtk::ResponseType::Ok);
 
     let (promise, future) = Promise::new();
     dlg.connect_response(move |dlg, answer| {
         dlg.hide();
-        let result = if answer == gtk::ResponseType::Ok {
+        let result = if answer == gtk::ResponseType::Accept {
             dlg.filename()
         } else {
             None
         };
         promise.fulfill(result);
     });
-    dlg.show_all();
+    dlg.show();
     future.await.unwrap_or(None)
 }
