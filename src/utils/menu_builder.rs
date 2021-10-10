c8151f2ac927bb5d7b7de2eb76901a95c283f6eb
Andrey Kutejko <andy128k@gmail.com>
2021-10-10 13:33:03 +0200

    remove unused code

--- src/utils/menu_builder.rs
+++ src/utils/menu_builder.rs
@@ -5,7 +5,6 @@ pub trait MenuItemBuilderExt {
     fn action(self, action: &str) -> Self;
     fn label(self, label: &str) -> Self;
     fn accel(self, accel: &str) -> Self;
-    fn icon(self, icon: &str) -> Self;
 }
 
 impl MenuItemBuilderExt for gio::MenuItem {
@@ -27,39 +26,16 @@ impl MenuItemBuilderExt for gio::MenuItem {
         self.set_attribute_value("accel", Some(&accel.to_variant()));
         self
     }
-
-    fn icon(self, icon: &str) -> Self {
-        match gio::Icon::for_string(icon) {
-            Ok(icon) => self.set_icon(&icon),
-            Err(e) => eprintln!("Cannot find icon {}. Reason: {}", icon, e),
-        }
-        self
-    }
 }
 
 pub trait MenuBuilderExt {
-    fn from_items(items: impl Iterator<Item = gio::MenuItem>) -> Self;
     fn item(self, item: gio::MenuItem) -> Self;
-    fn submenu(self, label: &str, submenu: gio::Menu) -> Self;
     fn section(self, section: gio::Menu) -> Self;
 }
 
 impl MenuBuilderExt for gio::Menu {
-    fn from_items(items: impl Iterator<Item = gio::MenuItem>) -> Self {
-        let menu = gio::Menu::new();
-        for item in items {
-            menu.append_item(&item);
-        }
-        menu
-    }
-
     fn item(self, item: gio::MenuItem) -> Self {
         self.append_item(&item);
-        self
-    }
-
-    fn submenu(self, label: &str, submenu: gio::Menu) -> Self {
-        self.append_submenu(Some(label), &submenu);
         self
     }
 
