ff4d0919e3e4d60b00ef447dfff3f859ab67cfe5
Andrey Kutejko <andy128k@gmail.com>
2022-02-12 04:51:01 +0100

    Update AES and CBC crates

--- src/format/revelation/crypto_container.rs
+++ src/format/revelation/crypto_container.rs
@@ -2,18 +2,17 @@ use super::utils::adjust_password;
 use crate::error::Result;
 use aes::{
     cipher::{
+        block_padding::Pkcs7,
         generic_array::{typenum::U16, GenericArray},
-        BlockDecrypt, BlockEncrypt, NewBlockCipher,
+        BlockDecrypt, BlockDecryptMut, BlockEncrypt, BlockEncryptMut, BlockSizeUser, KeyInit,
+        KeyIvInit,
     },
     Aes256,
 };
-use block_modes::{block_padding::Pkcs7, BlockMode, Cbc};
 use deflate::deflate_bytes_zlib;
 use inflate::inflate_bytes_zlib;
 use rand::random;
 use std::io::{Read, Write};
-
-type Cipher = Cbc<Aes256, Pkcs7>;
 
 pub fn decrypt(source: &mut dyn Read, password: &str) -> Result<Vec<u8>> {
     let mut iv = GenericArray::<u8, U16>::default();
@@ -21,16 +20,13 @@ pub fn decrypt(source: &mut dyn Read, password: &str) -> Result<Vec<u8>> {
 
     let mut encrypted_content = Vec::new();
     source.read_to_end(&mut encrypted_content)?;
-    if encrypted_content.len() % 16 != 0 {
-        return Err("Corrupted file: wrong size".into());
-    }
 
     let password = adjust_password(password).into();
 
     // decrypt the initial vector for CBC decryption
     Aes256::new(&password).decrypt_block(&mut iv);
-    let decrypted = Cipher::new_fix(&password, &iv)
-        .decrypt_vec(&encrypted_content)
+    let decrypted = cbc::Decryptor::<Aes256>::new(&password, &iv)
+        .decrypt_padded_mut::<Pkcs7>(&mut encrypted_content)
         .map_err(|_| "File cannot be decrypted")?;
     let data = inflate_bytes_zlib(&decrypted).map_err(|e| format!("Corrupted file: {}", e))?;
     Ok(data)
@@ -39,8 +35,13 @@ pub fn decrypt(source: &mut dyn Read, password: &str) -> Result<Vec<u8>> {
 pub fn encrypt(writer: &mut dyn Write, data: &[u8], password: &str) -> Result<()> {
     let password = adjust_password(password).into();
     let deflated = deflate_bytes_zlib(data);
+
+    let mut buffer = vec![0_u8; deflated.len() + cbc::Encryptor::<Aes256>::block_size()];
+
     let mut iv = random::<[u8; 16]>().into();
-    let encrypted = Cipher::new_fix(&password, &iv).encrypt_vec(&deflated);
+    let encrypted = cbc::Encryptor::<Aes256>::new(&password, &iv)
+        .encrypt_padded_b2b_mut::<Pkcs7>(&deflated, &mut buffer)
+        .map_err(|_| "File cannot be encrypted")?;
     Aes256::new(&password).encrypt_block(&mut iv);
     writer.write_all(&iv)?;
     writer.write_all(&encrypted)?;
