a5249ce099adcec4a9426b1dadf058db302eabf2
Andrey Kutejko <andy128k@gmail.com>
2024-01-01 23:53:58 +0100

    Revert "Replace embed-resource with winres" and update dependencies
    This reverts commit b4dfa537398a9d1a49d60e2ba4b2181d081cfd4c.

--- build.rs
+++ build.rs
@@ -2,8 +2,5 @@ fn main() {
     glib_build_tools::compile_resources(&["."], "icons.gresource.xml", "icons.gresource");
 
     #[cfg(target_os = "windows")]
-    winres::WindowsResource::new()
-        .set_icon("icons/app-icon/password-storage.ico")
-        .compile()
-        .unwrap();
+    embed_resource::compile("./icons.rc");
 }
