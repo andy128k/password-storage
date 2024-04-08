674ec5b734d539c422a3efce862df9f823f59f00
Andrey Kutejko <andy128k@gmail.com>
2024-04-08 10:49:11 +0200

    Run CI tests in a container and fix Windows build

--- build.rs
+++ build.rs
@@ -2,5 +2,5 @@ fn main() {
     glib_build_tools::compile_resources(&["."], "icons.gresource.xml", "icons.gresource");
 
     #[cfg(target_os = "windows")]
-    embed_resource::compile("./icons.rc");
+    embed_resource::compile("./icons.rc", embed_resource::NONE);
 }
