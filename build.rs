0953fbee375e010e3318d1a53bf66b0dd5084d81
Andrey Kutejko <andy128k@gmail.com>
2024-11-16 13:02:23 +0100

    Update dependencies

--- build.rs
+++ build.rs
@@ -2,5 +2,7 @@ fn main() {
     glib_build_tools::compile_resources(&["."], "icons.gresource.xml", "icons.gresource");
 
     #[cfg(target_os = "windows")]
-    embed_resource::compile("./icons.rc", embed_resource::NONE);
+    embed_resource::compile("./icons.rc", embed_resource::NONE)
+        .manifest_required()
+        .unwrap();
 }
