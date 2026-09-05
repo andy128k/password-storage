e0f01e81566ea070b3296b5a9a46fcd87736c343
Andrey Kutejko <andy128k@gmail.com>
2026-09-05 11:37:44 +0200

    Use cfg_select macro

--- src/compat/init.rs
+++ src/compat/init.rs
@@ -1,13 +1,14 @@
-#[cfg(not(target_os = "macos"))]
-pub fn init() {}
-
-#[cfg(target_os = "macos")]
 pub fn init() {
-    use gtk::prelude::*;
+    cfg_select! {
+        target_os = "macos" => {
+            use gtk::prelude::*;
 
-    let Some(settings) = gtk::Settings::default() else {
-        eprintln!("Failed to configure global settings: No default settings found.");
-        return;
-    };
-    settings.set_property("gtk-decoration-layout", "close,minimize,maximize");
+            if let Some(settings) = gtk::Settings::default() {
+                settings.set_property("gtk-decoration-layout", "close,minimize,maximize");
+            } else {
+                eprintln!("Failed to configure global settings: No default settings found.");
+            }
+        }
+        _ => {}
+    }
 }
